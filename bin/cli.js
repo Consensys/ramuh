8d475668cdf641d0f1a581a7c1c465e599e5af7b
Federico Gimenez <federico.gimenez@gmail.com>
2018-08-12 17:39:25 +0200

    basic entry point

---
+++ bin/cli.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+'use strict'
+
+const { getLogger } = require('../lib/logging')
+const os = require('os')
+
+const args = require('yargs')
+  .options({
+    'contractspath': {
+      describe: 'relative path to watch contract files',
+      default: path.join(__dirname, 'contracts')
+    },
+    'apiaddress': {
+      describe: 'Address of Mythril API',
+      default: 'localhost:3100'
+    },
+    'apikey': {
+      describe: 'API key for accessing Mytrhil',
+      default: 'localhost:3100'
+    },
+    'datadir': {
+      describe: 'Data directory to store status analysis',
+      default: `${os.homedir()}/Library/Ethereum`
+    },
+    'loglevel': {
+      describe: 'Logging verbosity',
+      choices: [ 'error', 'warn', 'info', 'debug' ],
+      default: 'info'
+    }
+  })
+  .locale('en_EN')
+  .argv
+const logger = getLogger({loglevel: args.loglevel})
+
+function run () {
+
+}
+
+try {
+  run()
+} catch(err) {
+  logger.error(err))
+}
