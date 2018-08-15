51000b4754547562d0c326fbe2d2f258b2ca31bd
Federico Gimenez <federico.gimenez@gmail.com>
2018-08-15 17:19:55 +0200

    only push .sol files

--- test/watcher.js
+++ test/watcher.js
@@ -5,31 +5,65 @@ const tmp = require('tmp')
 const path = require('path')
 const through = require('through')
 
-const tmpdir = tmp.dirSync().name
-const watcher = new Watcher(tmpdir)
-
 tape('[WATCHER]: observed files', t => {
   t.test('should pipe file name of new .sol files on creation', st => {
+    const tmpDir = tmp.dirSync().name
+    const watcher = new Watcher({tmpDir: tmpDir, pollingStep: 10})
+
     const ts = through(function write (data) {
       this.emit('data', data)
     })
-    watcher.pipe(ts)
 
-    const filePath = path.resolve(tmpdir, 'test.sol')
+    const filePath = path.resolve(tmpDir, 'test.sol')
     fs.writeFile(filePath, 'Hey there!', function (err) {
       st.error(err, 'writing file succeeded')
+
+      watcher.pipe(ts)
     })
 
     ts.on('data', function (data) {
       const actual = data.toString('utf8')
       st.equal(actual, filePath)
       watcher.stop()
+      st.end()
     })
-
-    st.end()
   })
 
   t.test('should not pipe file name of new files with non sol extension on creation', st => {
+    const tmpDir = tmp.dirSync().name
+    const watcher = new Watcher({tmpDir: tmpDir, pollingStep: 10})
+
+    const ts = through(function write (data) {
+      this.emit('data', data)
+    })
+
+    const badFileName = 'test.abc'
+    let fileNames = ['test1.sol', badFileName, 'test2.sol']
+    for (let i = 0; i < fileNames.length; i++) {
+      const filePath = path.resolve(tmpDir, fileNames[i])
+      fs.writeFile(filePath, 'Hey there!', function (err) {
+        st.error(err, `writing ${filePath} succeeded`)
+
+        if (i === fileNames.length - 1) {
+          watcher.pipe(ts)
+        }
+      })
+    }
+
+    let total = 0
+    const notExpected = path.resolve(tmpDir, badFileName)
+    ts.on('data', function (data) {
+      total++
+      const actual = data.toString('utf8')
+      st.notEqual(actual, notExpected, 'non .sol files should not be sent')
+      if (total === fileNames.length - 1) {
+        watcher.stop()
+        st.end()
+      }
+    })
+  })
+
+  t.test('should pipe file name of new .sol files on creation only once', st => {
     st.end()
   })
 
@@ -38,6 +72,10 @@ tape('[WATCHER]: observed files', t => {
   })
 
   t.test('should not pipe file name of new files with non sol extension on changes', st => {
+    st.end()
+  })
+
+  t.test('should pipe file name of new .sol files on changes only once', st => {
     st.end()
   })
 
