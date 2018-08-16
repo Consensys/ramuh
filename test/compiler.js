061c7d1ca8364660a33806394edf66dff1b35e95
Federico Gimenez <federico.gimenez@gmail.com>
2018-08-16 08:51:48 +0200

    inject solc dependency and use stub in test

--- test/compiler.js
+++ test/compiler.js
@@ -6,10 +6,23 @@ const tmp = require('tmp')
 const through = require('through')
 const { getLogger } = require('../lib/logging')
 const logger = getLogger({loglevel: 'error'})
+const sinon = require('sinon')
+const solc = require('solc')
 
 tape('[COMPILER]: compile files', t => {
   t.test('should return bytecode of files with a single contract', st => {
-    const compiler = new Compiler({logger: logger})
+    const tmpDir = tmp.dirSync().name
+    const filePath = path.resolve(tmpDir, 'test.sol')
+
+    const expectedBytecode = '6080604052348015600f57600080fd5b50603580601d6000396000f3006080604052600080fd00a165627a7a72305820dc80e598282646461f0b0d4e04097ad20ec3797452ca6ee933b63ad5aa24e3aa0029'
+    sinon.stub(solc, 'compile').returns({
+      contracts: {
+        ':Hello': {
+          bytecode: expectedBytecode
+        }
+      }
+    })
+    const compiler = new Compiler({logger: logger, solc: solc})
 
     const origin = through()
     const target = through(function write (data) {
@@ -18,8 +31,6 @@ tape('[COMPILER]: compile files', t => {
 
     origin.pipe(compiler).pipe(target)
 
-    const tmpDir = tmp.dirSync().name
-    const filePath = path.resolve(tmpDir, 'test.sol')
     const contractContent = `pragma solidity ^0.4.24;
 contract Hello {}
 `
@@ -35,9 +46,9 @@ contract Hello {}
       const expectedContract = ':Hello'
       st.equal(data.contracts[0].name, expectedContract)
 
-      const expectedBytecode = '6080604052348015600f57600080fd5b50603580601d6000396000f3006080604052600080fd00a165627a7a72305820dc80e598282646461f0b0d4e04097ad20ec3797452ca6ee933b63ad5aa24e3aa0029'
       st.equal(data.contracts[0].bytecode, expectedBytecode)
 
+      solc.compile.restore()
       st.end()
     })
   })
