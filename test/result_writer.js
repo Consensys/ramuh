624b69ffc0a510f1b838e66369b6473cf49e4120
Federico Gimenez <federico.gimenez@gmail.com>
2018-08-21 19:43:09 +0200

    basic ResultWriter functionality

---
+++ test/result_writer.js
@@ -0,0 +1,60 @@
+const tape = require('tape')
+const PassThrough = require('stream').PassThrough
+const { getLogger } = require('../lib/logging')
+const tmp = require('tmp')
+const fs = require('fs-extra')
+const path = require('path')
+
+const ResultWriter = require('../lib/result_writer')
+const logger = getLogger({loglevel: 'error'})
+
+tape('[WATCHER]: observed files', t => {
+  t.test('should pipe file name of new .sol files on creation', st => {
+    const tmpDir = tmp.dirSync().name
+    const contractName = 'test.sol'
+
+    const resultWriter = new ResultWriter({logger: logger, contractsPath: tmpDir})
+
+    const origin = PassThrough({objectMode: true})
+    const target = PassThrough({objectMode: true})
+
+    const expectedIssues = [
+      {
+        debug: 'debug1',
+        address: 'address1',
+        type: 'type1',
+        function: 'function1',
+        description: 'description1',
+        title: 'title1'
+      },
+      {
+        debug: 'debug2',
+        address: 'address2',
+        type: 'type2',
+        function: 'function2',
+        description: 'description2',
+        title: 'title2'
+      }
+    ]
+
+    target.on('data', (data) => {
+      const expectedResultsPath = path.resolve(tmpDir, contractName + '.mythril')
+      st.equal(data.results.path, expectedResultsPath)
+
+      const contents = fs.readFileSync(expectedResultsPath)
+      const contentObj = JSON.parse(contents)
+      st.deepEqual(expectedIssues, contentObj)
+
+      st.end()
+    })
+
+    origin.write({
+      filePath: path.resolve(tmpDir, contractName),
+      analysis: {
+        issues: expectedIssues
+      }
+    })
+
+    origin.pipe(resultWriter).pipe(target)
+  })
+})
