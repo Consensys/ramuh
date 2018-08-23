69f80e7875ef22e4995f77d671c716286e1498f3
Federico Gimenez <federico.gimenez@gmail.com>
2018-08-23 14:35:34 +0200

    added pipeline module

--- bin/cli.js
+++ bin/cli.js
@@ -2,13 +2,8 @@
 'use strict'
 
 const path = require('path')
-const url = require('url')
 
-const Watcher = require('../lib/watcher')
-const Compiler = require('../lib/compiler')
-const Requester = require('../lib/requester')
-const Poller = require('../lib/poller')
-const ResultWriter = require('../lib/result_writer')
+const Pipeline = require('../lib/pipeline')
 const { getLogger } = require('../lib/logging')
 
 const args = require('yargs')
@@ -37,48 +32,9 @@ const logger = getLogger({loglevel: args.loglevel})
 function run () {
   logger.info('Starting ithildin...')
 
-  // args validation
-  const apiUrl = url.parse(args.apiurl)
-  if (apiUrl.hostname === null) {
-    throw new Error(`${args.apiurl} is not a valid URL`)
-  }
+  const pipeline = new Pipeline(args.apiurl, args.apikey, args.contractspath, logger)
 
-  const watcher = new Watcher({logger: logger, contractsPath: args.contractspath})
-  const compiler = new Compiler({logger: logger})
-  const requester = new Requester({
-    logger: logger,
-    apiUrl: apiUrl,
-    apiKey: args.apikey})
-  const poller = new Poller({
-    logger: logger,
-    apiUrl: apiUrl,
-    apiKey: args.apikey})
-  const resultWriter = new ResultWriter({logger: logger})
-
-  const errorHandler = (err) => logger.error(err)
-  watcher.on('err', errorHandler)
-  compiler.on('err', errorHandler)
-  requester.on('err', errorHandler)
-  poller.on('err', errorHandler)
-  resultWriter.on('err', errorHandler)
-
-  // main pipeline, each step performs some required functionality and adds info
-  // to the object in transit.
-
-  // watcher looks into an specific directory and starts the pipeline, adds paths
-  // of .sol files created/changed.
-  watcher
-  // for each contract in each of the changed/added files, compiler compiles it
-  // and adds its name and bytecode to the object down the pipeline.
-    .pipe(compiler)
-  // requester sends an analysis request to the API and adds uuid of the
-  // requested analysis.
-    .pipe(requester)
-  // poller keeps querying the API until it gets the analysis results, then adds
-  // the returned issues to the in-transit object.
-    .pipe(poller)
-  // resultWriter writes down a file with the issues and adds its path.
-    .pipe(resultWriter)
+  pipeline.run()
 }
 
 try {
---
+++ test/integration/pipeline.js
@@ -0,0 +1,18 @@
+const tape = require('tape')
+/*
+const { PassThrough } = require('stream')
+const tmp = require('tmp')
+const fs = require('fs')
+const path = require('path')
+
+const ResultWriter = require('../../lib/result_writer')
+const { getLogger } = require('../../lib/logging')
+
+const logger = getLogger({loglevel: 'error'})
+*/
+
+tape('[PIPELINE]: basic functionality', t => {
+  t.test('result files should be writen', st => {
+    st.end()
+  })
+})
