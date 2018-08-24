0e117ddaceaf59a8cf636880c77f2adeeae7d994
Federico Gimenez <federico.gimenez@gmail.com>
2018-08-24 12:21:43 +0200

    notifier unit tests

--- test/unit/notifier.js
+++ test/unit/notifier.js
@@ -1,7 +1,79 @@
 const tape = require('tape')
+const nodeNotifier = require('node-notifier')
+const sinon = require('sinon')
+const { PassThrough } = require('stream')
+const Notifier = require('../../lib/notifier')
+const { getLogger } = require('../../lib/logging')
+
+const logger = getLogger({loglevel: 'err'})
+const filePath = 'file-path'
+const resultsPath = 'results-path'
 
 tape('[NOTIFIER]: general functionallity', t => {
-  t.test('should send notifications', st => {
+  t.test('setup', st => {
+    sinon.stub(nodeNotifier, 'notify')
+
+    st.end()
+  })
+  t.test('should send notifications on issues', st => {
+    const issueName = 'issue-name'
+
+    const notifier = new Notifier({logger: logger})
+
+    const origin = PassThrough({objectMode: true})
+    const target = PassThrough({objectMode: true})
+
+    origin.pipe(notifier).pipe(target)
+
+    origin.write({
+      filePath: filePath,
+      results: {
+        path: resultsPath
+      },
+      analysis: {
+        issues: [
+          {
+            name: issueName
+          }
+        ]
+      }
+    })
+
+    target.on('data', (data) => {
+      st.ok(data.notified)
+
+      st.end()
+    })
+  })
+
+  t.test('should not  send notifications on empty issues', st => {
+    const notifier = new Notifier({logger: logger})
+
+    const origin = PassThrough({objectMode: true})
+    const target = PassThrough({objectMode: true})
+
+    origin.pipe(notifier).pipe(target)
+
+    origin.write({
+      filePath: filePath,
+      results: {
+        path: resultsPath
+      },
+      analysis: {
+        issues: []
+      }
+    })
+
+    target.on('data', (data) => {
+      st.notok(data.notified)
+
+      st.end()
+    })
+  })
+
+  t.test('teardown', st => {
+    nodeNotifier.notify.restore()
+
     st.end()
   })
 })
